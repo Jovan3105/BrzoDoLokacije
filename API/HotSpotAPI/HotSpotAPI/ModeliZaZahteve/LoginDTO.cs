754c12cdb1de9cb024ce4f75ee5ca86d48df2b7f
Nemanja <nemanjamilanovic757@gmail.com>
2022-10-27 14:56:35 +0200

    Dodata mogucnost izmene podataka korisnika na back-u. Istestirano preko postmane. #2

--- API/HotSpotAPI/HotSpotAPI/ModeliZaZahteve/LoginDTO.cs
+++ API/HotSpotAPI/HotSpotAPI/ModeliZaZahteve/LoginDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace HotSpotAPI.ModeliZaZahteve
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotSpotAPI.ModeliZaZahteve
 {
     public class LoginDTO
     {
@@ -10,6 +12,22 @@
     {
         public string Message { get; set; } = string.Empty;
         public LoginDTO Data { get; set; } = null;
+    }
+
+    public class EditUser
+    {
+        [Required(ErrorMessage = "Nije unet username")]
+        [StringLength(maximumLength: 20, ErrorMessage = "Maksimalna duzina username-a je 20")]
+        public string Username { get; set; } = string.Empty;
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "Nije uneta validna Email adresa")]
+        public string Email { get; set; } = string.Empty;
+        public string OldPassword { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Nije uneta sifra")]
+        public string NewPassword { get; set; } = string.Empty;
+    }
+    public class messageresponse
+    {
+        public string message { get; set; } = string.Empty;
     }
 
 }
---
+++ API/HotSpotAPI/HotSpotAPI/Servisi/UserService.cs
@@ -0,0 +1,33 @@
+﻿using HotSpotAPI.Data;
+using System.Security.Claims;
+
+namespace HotSpotAPI.Servisi
+{
+    public interface IUserService
+    {
+        public int GetUserId();
+    }
+    public class UserService : IUserService
+    {
+        private readonly IHttpContextAccessor httpContext;
+        private MySqlDbContext context;
+
+        public UserService(IHttpContextAccessor httpContext, MySqlDbContext context)
+        {
+            this.httpContext = httpContext;
+            this.context = context;
+        }
+        public int GetUserId()
+        {
+            int rez = -1; // ako nije ulogovan vraca -1
+            if (httpContext.HttpContext.User.FindFirstValue("id") != null)
+            {
+                rez = int.Parse(httpContext.HttpContext.User.FindFirstValue("id"));
+                var dbid = context.Korisnici.Find(rez);
+                if (dbid == null)
+                    rez = -1;
+            }
+            return rez;
+        }
+    }
+}
