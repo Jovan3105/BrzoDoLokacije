26cabcc754813111bcd74d4bbe3597b640e88556
Jovan Brzakovic <jova3105@gmail.com>
2022-10-30 17:44:32 +0100

    Sredjena redirekcija #3

---
+++ HotSpot/app/src/main/java/imi/projekat/hotspot/ui/fragment_ProfilePage.kt
@@ -0,0 +1,13 @@
+package imi.projekat.hotspot.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import imi.projekat.hotspot.R
+
+
+import androidx.fragment.app.Fragment
+
+class FragmentAddPost:Fragment(R.layout.fragment_add_post) {
+}
