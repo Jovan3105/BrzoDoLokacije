033e7451417e8fd7902ff34a3e2beed8fd4441a2
Andrija Lazic <lazic.andrija223@gmail.com>
2022-11-26 12:33:33 +0100

    Fix za mapu #20

--- HotSpot/app/src/main/java/imi/projekat/hotspot/MapsActivity.kt
+++ HotSpot/app/src/main/java/imi/projekat/hotspot/MapsActivity.kt
@@ -5,6 +5,7 @@ import android.app.Dialog
 import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
@@ -12,6 +13,7 @@ import com.google.android.gms.common.api.GoogleApiActivity
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
@@ -21,22 +23,26 @@ import com.vmadalin.easypermissions.dialogs.SettingsDialog
 import imi.projekat.hotspot.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback,EasyPermissions.PermissionCallbacks {
-
-    private lateinit var mMap: GoogleMap
-private lateinit var binding: ActivityMapsBinding
+    private lateinit var mapView: MapView
+    private lateinit var binding: ActivityMapsBinding
+    private var grantedPermission=false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-     binding = ActivityMapsBinding.inflate(layoutInflater)
-     setContentView(binding.root)
-
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        mapView=findViewById(binding.mapView.id)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+
 
         requestLocPermission()
+
+        if(grantedPermission){
+            mapView.getMapAsync(this)
+            mapView.onCreate(savedInstanceState)
+        }
+
     }
 
     private fun hasLocationPersimision()=EasyPermissions.hasPermissions(
@@ -62,23 +68,7 @@ private lateinit var binding: ActivityMapsBinding
         EasyPermissions.onRequestPermissionsResult(1,permissions,grantResults,this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
 
     override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
         if(EasyPermissions.somePermissionPermanentlyDenied(this,perms)){
@@ -89,7 +79,9 @@ private lateinit var binding: ActivityMapsBinding
     }
 
     override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
-        checkGooglePlayServices();
+        if(checkGooglePlayServices()==false)
+            return
+        grantedPermission=true
     }
 
     private fun checkGooglePlayServices(): Boolean {
@@ -106,5 +98,58 @@ private lateinit var binding: ActivityMapsBinding
         return false
     }
 
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+//        mMap = googleMap
+//
+//        // Add a marker in Sydney and move the camera
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
 
 }
