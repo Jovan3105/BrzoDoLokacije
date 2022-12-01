1b6269682f15c5dca3c2dee0905a3c0b638922c4
Andrija Lazic <lazic.andrija223@gmail.com>
2022-12-01 17:55:04 +0100

    Dodate animacije za prebacivanje izmedju fragmenata i fix: -fix izgleda my profile fragmenta

--- HotSpot/app/src/main/java/imi/projekat/hotspot/MainActivity.kt
+++ HotSpot/app/src/main/java/imi/projekat/hotspot/MainActivity.kt
@@ -5,14 +5,20 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.provider.Settings
 import android.util.Log
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -26,6 +32,12 @@ import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import imi.projekat.hotspot.Dialogs.LoadingDialog
 import imi.projekat.hotspot.Ostalo.BaseResponse
 import imi.projekat.hotspot.Ostalo.UpravljanjeResursima
+import imi.projekat.hotspot.UI.HomePage.HomePageFragment
+import imi.projekat.hotspot.UI.HomePage.HomePageFragmentDirections
+import imi.projekat.hotspot.UI.Post.CreatePostFragment
+import imi.projekat.hotspot.UI.Post.CreatePostFragmentDirections
+import imi.projekat.hotspot.UI.Profile.MyProfileFragment
+import imi.projekat.hotspot.UI.Profile.MyProfileFragmentDirections
 import imi.projekat.hotspot.ViewModeli.MainActivityViewModel
 import imi.projekat.hotspot.databinding.ActivityMainBinding
 import kotlinx.coroutines.flow.collectLatest
@@ -43,21 +55,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
+
+
         setContentView(view)
+
+
 
         val navHostFragment=supportFragmentManager.findFragmentById(R.id.fragmentContainerViewMainActivity) as NavHostFragment
         navKontroler=navHostFragment.navController
 
         binding.bottomNavigationView2.setOnItemSelectedListener {
+            val fragmentInstance = (supportFragmentManager.findFragmentById(R.id.fragmentContainerViewMainActivity) as NavHostFragment).childFragmentManager.primaryNavigationFragment
+
+
             when (it.itemId) {
                 R.id.home -> {
-                    navKontroler.navigate(R.id.homePageFragment)
+                    if(fragmentInstance is CreatePostFragment)
+                        navKontroler.navigate(CreatePostFragmentDirections.actionCreatePostFragmentToHomePageFragment())
+                    if(fragmentInstance is MyProfileFragment)
+                        navKontroler.navigate(MyProfileFragmentDirections.actionMyProfileFragmentToHomePageFragment())
                 }
                 R.id.add -> {
-                    navKontroler.navigate(R.id.createPostFragment)
+                    if(fragmentInstance is HomePageFragment)
+                        navKontroler.navigate(HomePageFragmentDirections.actionHomePageFragmentToCreatePostFragment())
+                    if(fragmentInstance is MyProfileFragment)
+                        navKontroler.navigate(MyProfileFragmentDirections.actionMyProfileFragmentToCreatePostFragment())
                 }
                 R.id.profile -> {
-                    navKontroler.navigate(R.id.myProfileFragment)
+                    if(fragmentInstance is HomePageFragment)
+                        navKontroler.navigate(HomePageFragmentDirections.actionHomePageFragmentToMyProfileFragment())
+                    if(fragmentInstance is CreatePostFragment)
+                        navKontroler.navigate(CreatePostFragmentDirections.actionCreatePostFragmentToMyProfileFragment())
                 }
             }
             true
